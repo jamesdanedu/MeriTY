218c03fe4b2eb25b4cd5a5e6eec294237954ae64
jamesdanedu <121300535+jamesdanedu@users.noreply.github.com>
2025-04-26 22:36:29 +0100

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -36,6 +36,6 @@ class Settings(BaseSettings):
 
     class Config:
         case_sensitive = True
-        env_file = ".env"
+        env_file = ".env" if not os.getenv("VERCEL_ENV") else None
 
 settings = Settings()
