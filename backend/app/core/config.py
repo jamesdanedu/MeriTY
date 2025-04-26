f28728de5461de4c86b04c2ffc6916badf700459
jamesdanedu <121300535+jamesdanedu@users.noreply.github.com>
2025-04-26 21:47:37 +0100

    Add files via upload

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,37 @@
+# app/core/config.py
+import secrets
+from typing import Any, Dict, List, Optional, Union
 
+from pydantic import AnyHttpUrl, validator
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    # CORS allowed origins
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    # Supabase settings
+    SUPABASE_URL: str
+    SUPABASE_KEY: str
+    
+    # Project settings
+    PROJECT_NAME: str = "MeriTY Credits Tracker"
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+
+settings = Settings()
