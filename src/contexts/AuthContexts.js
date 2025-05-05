1ec775c8e1f21eed6263dd6428a631efdf6610f3
jamesdanedu <121300535+jamesdanedu@users.noreply.github.com>
2025-05-05 15:51:55 +0100

    Add files via upload

--- src/contexts/AuthContexts.js
+++ src/contexts/AuthContexts.js
@@ -1,1 +1,34 @@
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import { getSession } from '@/utils/auth';
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadUserFromSession() {
+      const { session } = getSession();
+      
+      if (session) {
+        setUser(session.user);
+      }
+      
+      setLoading(false);
+    }
+
+    loadUserFromSession();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
 
